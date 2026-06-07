741e79f5d7ec2226cc2d2722c8943496d897b741
noedsn <noedsond@gmail.com>
2026-06-06 23:15:13 -0300

    feat: adiciona exercicio do Funcionario

---
+++ src/main/java/org/example/ContaBancaria.java
@@ -0,0 +1,93 @@
+package org.example;
+
+
+    public class ContaBancaria {
+
+        private double saldo;
+        private double chequeEspecial;
+        private double chequeEspecialUsado;
+
+        public double getSaldo() {
+            return saldo;
+        }
+
+        public void setSaldo(double saldo) {
+            this.saldo = saldo;
+        }
+
+        public double getChequeEspecial() {
+            return chequeEspecial;
+        }
+
+        public void setChequeEspecial(double chequeEspecial) {
+            this.chequeEspecial = chequeEspecial;
+        }
+
+        public double getChequeEspecialUsado() {
+            return chequeEspecialUsado;
+        }
+
+        public void setChequeEspecialUsado(double chequeEspecialUsado) {
+            this.chequeEspecialUsado = chequeEspecialUsado;
+        }
+
+        public ContaBancaria(double depositoInicial) {
+            this.saldo = depositoInicial;
+            if (depositoInicial <= 500.00) {
+                this.chequeEspecial = 50.00;
+            } else {
+                this.chequeEspecial = depositoInicial * 0.5;
+            }
+        }
+
+        public void depositar(double valor) {
+            if (this.chequeEspecialUsado > 0) {
+                double taxa = this.chequeEspecialUsado * 0.20;
+                valor -= taxa;
+                System.out.printf("taxa de 20%% sobre o cheque especial cobrada: R$ %.2f\n", taxa);
+
+                if (valor >= chequeEspecialUsado) {
+                    this.saldo += (valor - chequeEspecialUsado);
+                    this.chequeEspecialUsado = 0.0;
+                } else {
+                    this.chequeEspecialUsado -= valor;
+                }
+            } else{
+                this.saldo += valor;
+            }
+        }
+
+        public void sacar(double valor) {
+            if (valor <= this.saldo) {
+                this.saldo -= valor;
+            } else if (valor <= (this.saldo + this.chequeEspecial)) {
+                double quantoFaltou = valor - this.saldo;
+                this.saldo = 0.0;
+                this.chequeEspecialUsado += quantoFaltou;
+            } else {
+                System.out.println("Saldo insuficiente");
+            }
+        }
+
+        public void pagarBoleto(double valor) {
+            sacar(valor);
+        }
+
+        public boolean isUsandoChequeEspecial() {
+            return this.chequeEspecialUsado > 0;
+        }
+
+        public void consultarSaldo() {
+            System.out.println("===SALDO ATUAL===");
+            System.out.printf("Saldo em conta: R$ %.2f\n", this.saldo);
+        }
+
+        public void consultarChequeEspecial() {
+            System.out.println("===CHEQUE_ESPECIAL===");
+            System.out.printf("Limite de Cheque Especial: R$ %.2f\n", this.chequeEspecial);
+            System.out.printf("Valor do Cheque Utilizado: R$ %.2f\n", this.chequeEspecialUsado);
+            double disponivelTotal = this.saldo + (this.chequeEspecial - this.chequeEspecialUsado);
+            System.out.printf("Total disponivel para saque: R$ %.2fn", disponivelTotal);
+        }
+    }
+
---
+++ src/main/java/org/example/ContaMain.java
@@ -0,0 +1,71 @@
+package org.example;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class ContaMain {
+    static void main() {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o depósito para criar a conta: R$ ");
+        double depositoInicial = sc.nextDouble();
+
+        ContaBancaria conta = new ContaBancaria(depositoInicial);
+
+        int opcao;
+
+        do {
+            System.out.println("\n=== MENU CONTA BANCÀRIA ===");
+            System.out.println("1. Consultar Saldo");
+            System.out.println("2. Consultar Cheque Especial");
+            System.out.println("3. Depositar Dinheiro");
+            System.out.println("4. Sacar Dinheiro");
+            System.out.println("5. Pagar Boleto");
+            System.out.println("6. Verificar Uso do cheque Especial");
+            System.out.println("0. Sair");
+            System.out.println("Escolha uma opção");
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    conta.consultarSaldo();
+                    break;
+                case 2:
+                    conta.consultarChequeEspecial();
+                    break;
+                case 3:
+                    System.out.print("Digite o valor do deposito: R$ ");
+                    double valorDep = sc.nextDouble();
+                    conta.depositar(valorDep);
+                    break;
+                case 4:
+                    System.out.println("Digite o valor do saque: R$ ");
+                    double valorSaque = sc.nextDouble();
+                    conta.sacar(valorSaque);
+                    break;
+                case 5:
+                    System.out.print("Digite o valor do boleto: R$ ");
+                    double valorBoleto = sc.nextDouble();
+                    conta.pagarBoleto(valorBoleto);
+                    break;
+                case 6:
+                    if (conta.isUsandoChequeEspecial()) {
+                        System.out.println("Atenção: Você está utilizando o cheque especial.");
+                    } else {
+                        System.out.println("Você não está utilizando o cheque especial.");
+                    }
+                    break;
+                case 0:
+                    System.out.println("Encerrando o sistema. Obrigado!");
+                    break;
+                default:
+                    System.out.println("Opção invalida! Tente novamente.");
+            }
+        } while (opcao != 0);
+        sc.close();
+    }
+}
